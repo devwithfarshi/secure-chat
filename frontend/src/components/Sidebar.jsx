88448a50f969d764ba83d5bfd0c64746c3cf304b
devwithfarshi <kingsalman0102@gmail.com>
2024-01-16 12:18:53 -0600

    Sidebar UI Complete

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,19 +5,30 @@ import {
   Contrast,
   Search,
 } from "@mui/icons-material";
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, Badge, IconButton, styled } from "@mui/material";
 const Sidebar = () => {
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: 8,
+      top: 10,
+      padding: ".6rem",
+      fontSize: "1.2rem",
+    },
+  }));
+
   return (
     <div className="sidebar-container">
       {/*============ Siderbar Header Start ========= */}
       <header className="sb-header">
         <IconButton>
           {/* We also add change or update profile function */}
-          <Avatar
-            alt="user name"
-            src="https://res.cloudinary.com/dc2lde0cr/image/upload/v1704392044/portfolio/h495i3hfsbb3qrukpe5a.png"
-            sx={{ width: 56, height: 56 }}
-          />
+          <StyledBadge badgeContent={10} max={9} color="primary">
+            <Avatar
+              alt="user name"
+              src="https://res.cloudinary.com/dc2lde0cr/image/upload/v1704392044/portfolio/h495i3hfsbb3qrukpe5a.png"
+              sx={{ width: 56, height: 56 }}
+            />
+          </StyledBadge>
         </IconButton>
         <IconButton>
           <Group sx={{ width: 36, height: 36 }} />
@@ -35,12 +46,29 @@ const Sidebar = () => {
       {/*============ Siderbar Header End ========= */}
       {/*============ Siderbar Search Start ========= */}
       <div className="sb-search">
-        <Search />
+        <Search sx={{ width: 24, height: 24 }} />
         <input type="text" name="search" placeholder="Search" />
       </div>
       {/*============ Siderbar Search End ========= */}
       {/*============ Siderbar Conversastins Start ========= */}
-      <div className="sb-conversations">code for Conversations </div>
+      <div className="sb-conversations">
+        <h4 className="conversations-heading">Your connections</h4>
+        <ul className="conversations-lists">
+          {/* <li className="conversations-list-item"> */}
+          <li className="conversations-list-item unreed">
+            <Avatar
+              alt="user name"
+              src="https://res.cloudinary.com/dc2lde0cr/image/upload/v1704392044/portfolio/h495i3hfsbb3qrukpe5a.png"
+              sx={{ width: 56, height: 56 }}
+            />
+            <div className="info">
+              <span className="userName">MD Salman Farshi</span>
+              {/* <p>You : hello salman</p> */}
+              <p className="unreed">You : hello salman</p>
+            </div>
+          </li>
+        </ul>
+      </div>
       {/*============ Siderbar Conversastins End ========= */}
     </div>
   );
